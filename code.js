3ea7c7f7f172e20cb3e79af71eb7996660c66ad2
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-08-10 16:51:02 -0500

    completed exercises 5 and 6

--- code.js
+++ code.js
@@ -12,10 +12,9 @@ function helloWorld() {
 // Exercise #2 Ensure our function returns the right data type.
 // Exercise #3 Add a test to confirm actual vs. expected output.
 // Exercise #4 Add another small, simple test
+// Exercise #5 One more test before refactoring...
+// Exercise #6 Implement the refactor!
+
 function sayHello(input) {
-    if (input === "Alex") {
-        return "Hello, Alex!"
-    } else {
-        return "Hello, Jane!";
-    } 
+    return `Hello, ${input}!`;
 }
